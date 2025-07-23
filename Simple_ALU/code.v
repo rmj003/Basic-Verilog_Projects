0e7c1cf5ec73129bf54c8e86b8d28bdfaebe5901
rmj003 <145194731+rmj003@users.noreply.github.com>
2025-07-23 14:00:13 +0530

    Update and rename Simple_ALU.v to Simple_ALU/code.v

--- Simple_ALU/code.v
+++ Simple_ALU/code.v
@@ -45,22 +45,3 @@ module alu(f,a,b,mode,op);
       endcase
    end
 endmodule
-
-
-//SAMPLE TESTBENCH
-module alutb;
- reg mode;
- reg [1:0]op;
- reg[7:0]a,b;
- wire [7:0]f;
- alu a1(f,a,b,mode,op);
- initial
-   begin
-    #10 mode=0;
-    op=2'b10;
-    a=8'd4;b=8'h2;
-    #10 mode=1;op=2'b01;
-    #10 mode=0;
-    #10 $finish;
-   end
-endmodule

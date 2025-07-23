002b2cf18da1cf6ef14b10de67ab9c3eb227f52c
rmj003 <145194731+rmj003@users.noreply.github.com>
2025-07-23 14:01:27 +0530

    Update and rename Up_Down_counter.v to Up_Down_counter/code.v

--- Up_Down_counter/code.v
+++ Up_Down_counter/code.v
@@ -32,21 +32,3 @@ module up_down_counter(count,clr,ld,clk,mode,din);
             else          count<=count-1;
         end
 endmodule
-
-
-//Sample testbench
-module up_down_counter_tb;
-    reg clr,mode,ld,clk; reg [7:0]din;
-    wire [7:0]count;
-    up_down_counter m1(count,clr,ld,clk,mode,din);
-    always #5 clk=~clk;
-    initial
-        begin
-            clk=1;
-            #2 clr=1'b1;ld=1'b0;mode=1'b0;din=8'b0;
-            #5 clr=1'b0;ld=1'b1;din=8'd100;
-            #10 ld=1'b0;
-            #20 ld=1'b0;mode=1'b1;
-            #30 $finish;
-        end    
-endmodule

302b0a4a1554b3222b5a4e03f3659f8b74e88848
rmj003 <145194731+rmj003@users.noreply.github.com>
2025-07-23 13:52:13 +0530

    Create testbench.v

---
+++ MUX_16to1/testbench.v
@@ -0,0 +1,18 @@
+//Testbench for the above multiplexer
+module mux_tb();
+reg [15:0]A;
+reg [3:0]S;
+wire F;
+mux16x1 m1(.in(A),.sel(S),.out(F));
+initial
+ begin
+     $dumpfile("mux.vcd");
+     $dumpfile(0);
+     $monitor($time,"A=%h,S=%h,F=%b",A,S,F);
+     #5 A=16'h33ff;S=4'h3;
+     #5 S=4'ha;
+     #5 S=4'h6;
+     #5 S=4'hc;
+     #5 $finish;
+ end
+endmodule

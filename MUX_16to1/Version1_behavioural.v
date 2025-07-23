39d68af270f84de8eaa202dcccf2555ca8768fe6
rmj003 <145194731+rmj003@users.noreply.github.com>
2025-07-23 13:51:47 +0530

    Update and rename MUX_16to1.v to MUX_16to1/Version1_behavioural.v

--- MUX_16to1/Version1_behavioural.v
+++ MUX_16to1/Version1_behavioural.v
@@ -1,22 +1,3 @@
-//Testbench for the above multiplexer
-module mux_tb();
-reg [15:0]A;
-reg [3:0]S;
-wire F;
-mux16x1 m1(.in(A),.sel(S),.out(F));
-initial
- begin
-     $dumpfile("mux.vcd");
-     $dumpfile(0);
-     $monitor($time,"A=%h,S=%h,F=%b",A,S,F);
-     #5 A=16'h33ff;S=4'h3;
-     #5 S=4'ha;
-     #5 S=4'h6;
-     #5 S=4'hc;
-     #5 $finish;
- end
-endmodule
-
 //VERSION 1: code for implementing 16x1 multiplexer(BEHAVIOURAL 16X1 Mux)
 module mux16x1(out,sel,in);
 input [15:0]in; input [3:0]sel;

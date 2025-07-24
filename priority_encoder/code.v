f91484bcfe2e386fa8db129e49d868dc335a47cf
rmj003 <145194731+rmj003@users.noreply.github.com>
2025-07-24 14:16:00 +0530

    Update and rename priority_encoder.v to priority_encoder/code.v

--- priority_encoder/code.v
+++ priority_encoder/code.v
@@ -38,16 +38,4 @@ module priority_encoder(in,code);
 endmodule
 
 
-//SAMPLE TESTBENCH
-module priority_encoder_tb;
-    reg [7:0]in;
-    wire[2:0]code;
-    priority_encoder m1(in,code);
-    initial
-        begin
-            #1 in=8'd1;
-            #5 in=8'd200;
-            #5 in=8'd100;
-            #5 $finish;
-        end
-endmodule
+
